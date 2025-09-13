533c68dd23c18acf35bf8ac95e8324ac22b800d3
M4ttH3W711 <matthew711.uni@gmail.com>
2025-09-13 17:55:08 -0600

    Pages created: home and shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>Sitio web de demostración - FIFA 2026</h1>
+      <h1>Página de Inicio "home"</h1>
     </>
   );
 }
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+    <>
+        <h1>Página de "Shop"</h1>
+    </>
+    )
+}
+
+export default page
